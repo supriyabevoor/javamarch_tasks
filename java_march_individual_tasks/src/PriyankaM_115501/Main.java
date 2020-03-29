fceacb0c028887e2adc5d09dd8055a65822f6d47
ASUS <ASUS@DESKTOP-RL23T4D>
2020-03-29 14:17:14 +0530

    PriyankaM_115501 completed task-20

---
+++ java_march_individual_tasks/src/PriyankaM_115501/Main.java
@@ -0,0 +1,48 @@
+package PriyankaM_115501;
+
+import java.util.Scanner;
+
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[]args) {
+		
+		Scanner sc =new Scanner(System.in);
+	
+		
+		System.out.println("Enter IP: ");
+			System.out.println(UserMainCode.ipValidator(sc.next()));
+	}
+}
+
+
+class UserMainCode {
+	
+static int ipValidator(String ip) {
+	
+	int flag=2;
+	
+	
+	StringTokenizer tokenizedIp=new StringTokenizer(ip,".");
+	
+	while(tokenizedIp.hasMoreTokens()) {
+		
+		int x=Integer.parseInt(tokenizedIp.nextToken());
+		
+		if( x>=0 && x<=255 ) {
+			
+			flag= 1;
+		}
+		else {
+			
+			return flag= 2;
+		}
+	}
+
+	return flag;
+}
+	
+	
+}
